0ab0156d972e199e7d1c30562159f87d7452af9e
jugalmehta <jugalmehta_7@yahoo.com>
2026-09-22 09:30:57 +0200

    Delete components directory

--- components/ui/Container.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export function Container({ children, className = "" }: { children: ReactNode; className?: string }) {
-  return <div className={`container-content ${className}`}>{children}</div>;
-}
